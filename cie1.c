8f9a49a3dd50130052803c51810917fef7114ba5
Manish-03-web <manish.22cs091@sode-edu.in>
2025-08-20 14:10:02 +0530

    Add files via upload

---
+++ cie1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low -1;
+    for(int j=low; j<=high-1; j++) {
+        if(arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i+1], &arr[high]);
+    return (i+1);
+}
+void quickSort(int arr[], int low, int high) {
+    if(low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi-1);
+        quickSort(arr, pi+1, high);
+    }
+}
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int *arr = (int*) malloc(n * sizeof(int));
+    if(arr == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+    printf("Enter %d integers:\n", n);
+    for(int i=0; i<n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    clock_t start = clock();
+    quickSort(arr, 0, n-1);
+    clock_t end = clock();
+    double time_taken = ((double)(end - start))/CLOCKS_PER_SEC;
+    printf("Sorted array:\n");
+    for(int i=0; i<n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    printf("Execution time for sorting: %f seconds\n", time_taken);
+    free(arr);
+    return 0;
+}
